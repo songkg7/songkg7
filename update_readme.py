714165c1a8f9f98012728f15fb37ac8e1af22a71
kgsong <kyunggeun.song@seavantage.com>
2022-06-10 11:34:02 +0900

    Update README.md

--- update_readme.py
+++ update_readme.py
@@ -17,8 +17,6 @@ markdown_text = """
 
 ## 🛠 Skills
 
-<br>
-
 <!--
 **songkg7/songkg7** is a ✨ _special_ ✨ repository because its `README.md` (this file) appears on your GitHub profile.
 
@@ -43,8 +41,8 @@ Here are some ideas to get you started:
 
 ![Aws](https://img.shields.io/badge/Amazon&nbsp;AWS-232F32?style=flat-square&logo=amazon-aws&logoColor=white)
 ![Elastic](https://img.shields.io/badge/Elastic-005571?style=flat-square&logo=elastic&logoColor=white)
-![PostgreSQL](https://img.shields.io/badge/postgreSQL-336791?style=flat-square&logo=postgresql&logoColor=white)
-![Docker](https://img.shields.io/badge/docker-2496ED?style=flat-square&logo=docker&logoColor=white)
+![PostgreSQL](https://img.shields.io/badge/PostgreSQL-336791?style=flat-square&logo=postgresql&logoColor=white)
+![Docker](https://img.shields.io/badge/Docker-2496ED?style=flat-square&logo=docker&logoColor=white)
 
 ### 📧 Contact & About Me
 
