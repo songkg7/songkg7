5b29c5a84e5b8009e0d8f85ca89dd17c431c4ff4
kgsong <kyunggeun.song@seavantage.com>
2022-11-08 15:35:12 +0900

    delete solved.ac badge
    - 랭크 업 이후 추가할 예정

--- update_readme.py
+++ update_readme.py
@@ -54,8 +54,6 @@ I'm haril song, a software engineer from South Korea. I'm interested in web deve
 <!--START_SECTION:waka-->
 <!--END_SECTION:waka-->
 
-[![Solved.ac 프로필](http://mazassumnida.wtf/api/v2/generate_badge?boj=songkg7)](https://solved.ac/songkg7)
-
 <p>
   <img height="180em" src="https://github-readme-stats.vercel.app/api?username=songkg7&show_icons=true&include_all_commits=true&bg_color=30,e96443,904e95&title_color=fff&text_color=fff">
 </p>
