e26b0435e756bca7d39e7f00e6e748b9d09ee239
kgsong <kyunggeun.song@seavantage.com>
2023-01-19 17:12:14 +0900

    chore: deploy own vercel instance for github readme stats

--- update_readme.py
+++ update_readme.py
@@ -65,7 +65,7 @@ I'm haril song, a software engineer from South Korea. I'm interested in web deve
 <!--END_SECTION:waka-->
 
 <p>
-  <img height="180em" src="https://github-readme-stats.vercel.app/api?username=songkg7&show_icons=true&include_all_commits=true&bg_color=30,e96443,904e95&title_color=fff&text_color=fff">
+  <img height="180em" src="https://github-readme-stats-liart-gamma.vercel.app/api?username=songkg7&show_icons=true&include_all_commits=true&bg_color=30,e96443,904e95&title_color=fff&text_color=fff">
 </p>
 
 """
