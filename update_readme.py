897224cc955883229069c4de0cfac867cb80ebb1
haril song <songkg7@gmail.com>
2024-07-05 14:56:43 +0900

    Revert "update script"
    This reverts commit 3e613c8433dc00578936cdf8d40498573e8ad989.

--- update_readme.py
+++ update_readme.py
@@ -11,7 +11,7 @@ for idx, feed in enumerate(rss_feed['entries']):
     if idx > MAX_POST_NUM:
         break
     feed_date = feed['published_parsed']
-    latest_blog_post_list += f"- [{feed_date.tm_year}/{feed_date.tm_mon}/{feed_date.tm_mday} - {feed['title']}]({feed['link']})\n"
+    latest_blog_post_list += f"- [{feed_date.tm_year}/{feed_date.tm_mon}/{feed_date.tm_mday} - {feed['title']}]({feed['link']}) <br>\n"
 
 markdown_text = """
 
